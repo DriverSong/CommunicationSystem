b1be8cce9e24cff320d3887d1027789fa36491d1
Qiujiawei <qiujiaweirenyang@qq.com>
2018-06-05 15:38:31 +0800

    新增RSA加解密

---
+++ src/com/test/RSATest.java
@@ -0,0 +1,32 @@
+package com.test;
+
+import com.util.asymmetry.RSA;
+
+public class RSATest {
+	public static void main(String args[]) {
+		//encode
+		System.out.println("input: aaa");
+		RSA rsaEn = new RSA("aaa", "aaa");
+		rsaEn.generate();
+		String pub1 = rsaEn.getIdRsaPub();
+		String pri1 = rsaEn.getIdRsa();
+		rsaEn.encode();
+		String output = rsaEn.getOutput();
+		System.out.println("output: "+ output);
+		
+		//Decode
+		RSA rsaDe = new RSA(output, "aaa");
+		rsaDe.generate();
+		String pub2 = rsaDe.getIdRsaPub();
+		String pri2 = rsaDe.getIdRsa();
+		System.out.println(pub1);
+		System.out.println(pub2);
+		System.out.println(pub1.length());
+		System.out.println(pub2.length());
+		System.out.println("pub : " + (pub1.equals(pub2)));
+		System.out.println("pri : " + (pri1.equals(pri2)));
+		rsaDe.decode();
+		String input = rsaDe.getOutput();
+		System.out.println("input1: " + input);
+	}
+}
--- src/com/util/asymmetry/RSA.java
+++ src/com/util/asymmetry/RSA.java
@@ -1,37 +1,109 @@
 package com.util.asymmetry;
 
+import java.security.Key;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+
 import com.model.Algorithm;
+import com.util.TypeConverse;
 
 public class RSA {
+	private String seed;
 	private String input;
 	private String idRsa;
 	private String idRsaPub;
 	private String output;
+	private Map<String, Key> rsaMap = new HashMap<>();
+	private static int KEYSIZE = 1024;
 	
 	public RSA() {
 	}
 	
 	public RSA(Algorithm algorithm) {
 		this.input = algorithm.getHashArea();
+		this.seed = algorithm.getRsaInput();
+	}
+	
+	public RSA(String input, String seed) {
+		this.input = input;
+		this.seed = seed;
 	}
 	
 	public void setAlgorithm (Algorithm algorithm) {
+		this.seed = algorithm.getRsaInput();
 		this.input = algorithm.getHashArea();
 	}
 	
 	public void generate() {
-		this.idRsa = "idRsa" + new Random().nextInt(100);
-		this.idRsaPub = "idRsaPub" + new Random().nextInt(100);
+		try {
+			byte[] seedBytes = seed.getBytes();
+			SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
+			//需要在getInstance后，再setSeed，才能根据seed生成伪随机数
+			sr.setSeed(seedBytes);
+			KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+			kpg.initialize(KEYSIZE, sr);
+			KeyPair kp = kpg.generateKeyPair();
+			//pub
+			Key pub = kp.getPublic();
+			rsaMap.put("pub", pub);
+			byte[] pubBytes = pub.getEncoded();
+			idRsaPub = TypeConverse.bytes2HexString(pubBytes);
+			//pri
+			Key pri = kp.getPrivate();
+			rsaMap.put("pri", pri);
+			byte[] priBytes = pri.getEncoded();
+			idRsa = TypeConverse.bytes2HexString(priBytes);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void encode() {
-		this.output = "encode" + this.input;
+		try {
+//			KeySpec kp = new X509EncodedKeySpec(TypeConverse.hexString2Bytes(idRsaPub));
+//			KeyFactory kf = KeyFactory.getInstance("RSA");
+//			PublicKey pub = kf.generatePublic(kp);
+			Key pub = rsaMap.get("pub");
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.ENCRYPT_MODE, pub);
+			byte[] inputBytes = input.getBytes();
+			byte[] outputBytes = cipher.doFinal(inputBytes);
+			System.out.println(Arrays.toString(outputBytes));
+			output = TypeConverse.bytes2HexString(outputBytes);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	public void decode() {
-		this.output = "decode" + this.output;
+		try {
+//			KeySpec kp = new PKCS8EncodedKeySpec(TypeConverse.hexString2Bytes(idRsa));
+//			KeyFactory kf = KeyFactory.getInstance("RSA");
+//			PrivateKey pri = kf.generatePrivate(kp);
+			Key pri = rsaMap.get("pri");
+			Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+			cipher.init(Cipher.DECRYPT_MODE, pri);
+			byte[] inputBytes = TypeConverse.hexString2Bytes(input);
+			System.out.println(Arrays.toString(inputBytes));
+			byte[] outputBytes = cipher.doFinal(inputBytes);
+			output = new String(outputBytes);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public String getInput() {
